1498d2685e680019357b4c222ac7c7f49328af46
Emily Arai <emilyarai@gmail.com>
2023-03-02 16:17:04 -0500

    Added LocationVO and Hats models, created poller

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200)
+    closet_name = models.CharField(max_length=100)
+
+
+class Hats(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    fabric = models.CharField(max_length=200, null=True)
+    style_name = models.CharField(max_length=200, null=True)
+    color = models.CharField(max_length=200, null=True)
+    picture_url = models.CharField(max_length=250, null=True)
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.style_name
+
+    def get_api_url(self):
+        return reverse("api_show_hat", kwargs={"id": self.id})
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from models import LocationVO, Hats
 
-# Create your views here.
+from common.json import ModelEncoder
+
+
+class LocationVoDetailEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["closet_name", "import_href"]
+
+
+class HatListEncoder(ModelEncoder):
+    model = Hats
+    properties = ["name"]
