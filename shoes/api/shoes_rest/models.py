b9a61f1195338c7d283631b21b30698c366c4e5e
Diana <dianangaritah@gmail.com>
2023-03-02 19:36:23 -0600

    Changes before I continue with the view functions

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class BinVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    closet_name = models.CharField(max_length=200, null=True, blank=True)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
+
+
+class Shoes(models.Model):
+    brand = models.CharField(max_length=100, null=True, blank=True)
+    model_name = models.CharField(max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    style = models.CharField(max_length=50)
+    photo_url = models.URLField(null=True)
+    bin_storage = models.ForeignKey(BinVO, related_name="shoes", on_delete=models.CASCADE)
+
+    def get_api_url(self):
+        return reverse("api_show_shoes", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.name
--- shoes/poll/poller.py
+++ shoes/poll/poller.py
@@ -10,14 +10,24 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoes_project.settings")
 django.setup()
 
 # Import models from hats_rest, here.
-# from shoes_rest.models import Something
+from shoes_rest.models import BinVO
+
+def get_bin():
+    response = requests.get("http://wardrobe-api:8000/api/bins/")
+    print("Response:", response)
+    content = json.loads(response.content)
+    print("content", content)
+    for bin_storage in content["bin"]:
+        BinVO.objects.update_or_create(
+            import_href=bin_storage["href"],
+            defaults={"closet_name": bin_storage["closet_name"]},
+        )
 
 def poll():
     while True:
         print('Shoes poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
+            get_bin()
         except Exception as e:
             print(e, file=sys.stderr)
         time.sleep(60)
